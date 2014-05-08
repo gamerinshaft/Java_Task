d483874d27f7272d9eee283090721a2654ed205d
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-05-08 20:33:24 +0900

    pieGraphの編集

--- PieGraph.java
+++ PieGraph.java
@@ -42,6 +42,7 @@ public class PieGraph extends Applet {
     g.fillOval(15+80, 15+5, 240, 240);
 
     //一番外枠の表示
+
     g.setColor(new Color(255,0,0));
     g.fillArc(15+80,15+5,240,240,90,360);
     g.setColor(new Color(255,128,0));
@@ -52,15 +53,18 @@ public class PieGraph extends Applet {
     g.fillArc(15+80,15+5,240,240,90,225);
     g.setColor(new Color(0,255,255));
     g.fillArc(15+80,15+5,240,240,90,180);
-    g.setColor(new Color(00,128,255));
+    g.setColor(new Color(0,128,255));
     g.fillArc(15+80,15+5,240,240,90,135);
     g.setColor(new Color(0,0,255));
     g.fillArc(15+80,15+5,240,240,90,90);
     g.setColor(new Color(0,0,128));
     g.fillArc(15+80,15+5,240,240,90,45);
 
-    g.setColor(new Color(35,35,35));
-    g.fillOval(15+80+10,15+5+10,220,220);
+    g.setColor(new Color(255,255,255));
+    g.fillOval(15+80+8,15+5+8,224,224);
+
+    g.setColor(new Color(255,255,255));
+    g.fillOval(15+80+9,15+5+9,222,222);
 
     // g.setColor(new Color(190,0,0));
     // g.fillArc(15+80+10,15+5+10,220,220,90,Math.round(360*pa[7]/max));
@@ -93,10 +97,52 @@ public class PieGraph extends Applet {
     g.setColor(new Color(25,25,25));
     g.fillOval(15+140, 15+65, 120, 120);
 
+    Graphics2D g2 = (Graphics2D)g;
+    // //うしろぼやけ
+    // float[] dist = {0.4f, 0.6f, 0.9f};
+    // Color[] colors = {new Color(128,128,0),new Color(64,64,0),new Color(5,5,5,80)};
+    //  RadialGradientPaint rgp =
+    //     new RadialGradientPaint( 215, 140, 150, dist, colors);
+    // g2.setPaint(rgp);
+    // g.fillOval(15+80+10,15+5+10,220,220);
+
+    for(i=count-1;i>=0;i--){
+      if(105+45-(90*i/(count-1))/2<135){
+        g.setColor(new Color(25,25,25));
+      }else if(105+45-(90*i/(count-1))/2<155){
+        g.setColor(new Color(35,35,35));
+      }else{
+        g.setColor(new Color(25,25,25));
+      }
+      g.fillOval(105+45-(90*i/(count-1))/2,30+45-(90*i/(count-1))/2,130+90*i/(count-1),130+90*i/(count-1));
+      if(pa[i]*100/max > 87.5){
+        g.setColor(new Color(255,0,0));
+      }else if(pa[i]*100/max > 75.0){
+        g.setColor(new Color(255,128,0));
+      }else if(pa[i]*100/max > 62.5){
+        g.setColor(new Color(255,255,0));
+      }else if(pa[i]*100/max > 50.0){
+        g.setColor(new Color(0,255,0));
+      }else if(pa[i]*100/max > 37.5){
+        g.setColor(new Color(0,255,255));
+      }else if(pa[i]*100/max > 25.0){
+        g.setColor(new Color(00,128,255));
+      }else if(pa[i]*100/max > 12.5){
+        g.setColor(new Color(0,0,255));
+      }else{
+        g.setColor(new Color(0,0,128));
+      }
+      g.fillArc(105+45-(90*i/(count-1))/2,30+45-(90*i/(count-1))/2,130+90*i/(count-1),130+90*i/(count-1),90,Math.round(360*pa[i]/max));
+      g.drawString(i+"",15+15+325,45+20+15*i);
+      g.drawString((int)(100*pa[i]/max)+"%",15+35+325,45+20+15*i);
+
+    }
+
     //メーター
     int j;
     int arc1 = 90;
     int length;
+
     //文面の記述
     g.setColor(new Color(255,255,255));
     g.drawString("各要素の",15+12,30);
@@ -105,16 +151,25 @@ public class PieGraph extends Applet {
     g.drawString("対する比率",15+5+325,30+15);
 
     //内側のメーター
-    for(j=0;j<8;j++){
+    for(j=0;j<count;j++){
+      float percent = pa[j]*100/max;
       length = Math.round(360*pa[j]/sum);
-      if(j<=1){
-        g.setColor(new Color(255,127*j,0));
-      }else if(j<=3){
-        g.setColor(new Color(255-127*(j-2),255,0));
-      }else if(j<=5){
-        g.setColor(new Color(0,255-127*(j-4),127*(j-4)));
+      if(percent > 87.5){
+        g.setColor(new Color(155+50+(int)(50*percent/100),0+(int)(50*percent/100),0+(int)(50*percent/100)));
+      }else if(percent > 75.0){
+        g.setColor(new Color(155+50+(int)(50*percent/100),28+50+(int)(50*percent/100),0+(int)(50*percent/100)));
+      }else if(percent > 62.5){
+        g.setColor(new Color(155+50+(int)(50*percent/100),155+50+(int)(50*percent/100),0+(int)(50*percent/100)));
+      }else if(percent > 50.0){
+        g.setColor(new Color(0+50+(int)(50*percent/100),155+50+(int)(50*percent/100),0+(int)(50*percent/100)));
+      }else if(percent > 37.5){
+        g.setColor(new Color(0+50+(int)(50*percent/100),155+50+(int)(50*percent/100),155+50+(int)(50*percent/100)));
+      }else if(percent > 25.0){
+        g.setColor(new Color(0+50+(int)(50*percent/100),28+50+(int)(50*percent/100),155+50+(int)(50*percent/100)));
+      }else if(percent > 12.5){
+        g.setColor(new Color(0+50+(int)(50*percent/100),0+(int)(50*percent/100),155+50+(int)(50*percent/100)));
       }else{
-        g.setColor(new Color(0,255-127*(j-5),255));
+        g.setColor(new Color(0+50+(int)(50*percent/100),0+(int)(50*percent/100),28+50+(int)(50*percent/100)));
       }
       g.drawString(j+"",15+15,45+20+15*j);
       g.drawString((int)(pa[j])+"",15+35,45+20+15*j);
